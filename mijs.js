4c2c39089f683c1838be5e747989b56cfba3a596
H3tshepswt <93703638+H3tshepswt@users.noreply.github.com>
2021-11-06 13:25:55 +0100

    Inicio

---
+++ mijs.js
@@ -0,0 +1,77 @@
+function menu()
+          {
+            let menu = document.getElementById("menu");
+            
+            
+            if(menu.style.visibility == "hidden")
+            {
+
+                menu.style.visibility = "visible";
+            }
+            
+            else
+            {
+              menu.style.visibility = "hidden";
+            }
+            
+          }
+
+
+          function texto()
+          {
+            let imagen= document.getElementById("imagen");
+            let txt=document.getElementById("txt");
+  
+            if(imagen.style.display == "none")
+            {
+              imagen.style.display="block";
+              txt.style.display="none";
+              
+            }
+            else
+            {
+              imagen.style.display="none";
+              txt.style.display="block";
+              
+            }
+  
+          }
+          function texto2()
+          {
+            let imagen= document.getElementById("imagen2");
+            let txt=document.getElementById("txt2");
+  
+            if(imagen.style.display == "none")
+            {
+              imagen.style.display="block";
+              txt.style.display="none";
+              
+            }
+            else
+            {
+              imagen.style.display="none";
+              txt.style.display="block";
+              
+            }
+  
+          }
+  
+          function texto3()
+          {
+            let imagen= document.getElementById("imagen3");
+            let txt=document.getElementById("txt3");
+  
+            if(imagen.style.display == "none")
+            {
+              imagen.style.display="block";
+              txt.style.display="none";
+              
+            }
+            else
+            {
+              imagen.style.display="none";
+              txt.style.display="block";
+              
+            }
+  
+          }
